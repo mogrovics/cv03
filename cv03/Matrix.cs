52ad4c5c70cd9d8d75034690ef6b7a1f28be463a
mogrovics <alex.mogrovics@gmail.com>
2021-02-24 12:05:56 +0100

    Add project files.

---
+++ cv03/Matrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cv03
+{
+    public class Matrix
+    {
+        double[,] matrix;
+
+        //Matrix constructor, takes matrix as an arg and makes an association with particular instance using "this" keyword
+        public Matrix(double[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+
+        //operator implementation via:
+        //https://www.dotnetperls.com/operator
+
+        public static Matrix operator +(Matrix a, Matrix b)
+        {
+            // number of elements in a column
+            int col_size = a.matrix.GetLength(0);
+            
+            // number of elements in a row
+            int row_size = a.matrix.GetLength(1);
+
+            //temporary result matrix with the same size as matrix "a"
+            var tmp = new Matrix(new double[col_size, row_size]);
+
+            //assign addition result to tmp
+            for (int i = 0; i < col_size; i++)
+            {
+                for (int j = 0; j < row_size; j++)
+                {
+                    tmp.matrix[i, j] = a.matrix[i, j] + b.matrix[i, j];                 }
+            }
+            
+            return tmp;
+        }
+    }
+}
